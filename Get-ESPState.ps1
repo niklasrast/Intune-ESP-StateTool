6d1e893ea8ffee8a56a5f5d19637ec74704fc74f
Niklas Rast <47035125+niklasrast@users.noreply.github.com>
2023-09-04 14:47:48 +0200

    Update Get-ESPState.ps1

--- Get-ESPState.ps1
+++ Get-ESPState.ps1
@@ -13,13 +13,10 @@
 #>
 
 #ESP check
-$ExplorerProcesses = @(Get-CimInstance -ClassName 'Win32_Process' -Filter "Name like 'explorer.exe'" -ErrorAction 'Ignore')
-foreach ($TargetProcess in $ExplorerProcesses) {
-    $Username = (Invoke-CimMethod -InputObject $TargetProcess -MethodName GetOwner).User
+$processesExplorer = @(Get-CimInstance -ClassName 'Win32_Process' -Filter "Name like 'explorer.exe'" -ErrorAction 'Ignore')
+$esp = $false
+foreach ($processExplorer in $processesExplorer) {
+    $user = (Invoke-CimMethod -InputObject $processExplorer -MethodName GetOwner).User
+    if ($user -eq 'defaultuser0' -or $user -eq 'defaultuser1') {$esp = $true}
 }
-if ($UserName -ne 'defaultuser0') {
-    Write-Host "ESP off"
-}
-else {
-    Write-Host "ESP on"
-}
+Write-Host $esp 
